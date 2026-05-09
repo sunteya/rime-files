b07e97bb581d79dc947113ea08b138690077b77f
sunteya <sunteya@gmail.com>
2026-05-09 23:21:25 +0800

    添加同步脚本

---
+++ src/copy-rime-ice.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -x
+
+root="$(cd "$(dirname "$0")/.." && pwd)"
+src="$root/src"
+rime_ice="$src/rime-ice"
+
+rsync -aL \
+  --exclude='.git/' \
+  --exclude='.github/' \
+  --exclude='.gitignore' \
+  --exclude='custom_phrase.txt' \
+  --exclude='default.yaml' \
+  --exclude='double_pinyin*.schema.yaml' \
+  --exclude='go.work' \
+  --exclude='LICENSE' \
+  --exclude='others/' \
+  --exclude='README.md' \
+  --exclude='squirrel.yaml' \
+  --exclude='t9.schema.yaml' \
+  --exclude='weasel.yaml' \
+  "$rime_ice/" \
+  "$root/"
