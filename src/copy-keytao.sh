595dfe884059b7a8ab630d55e6cb26f83ee22403
sunteya <sunteya@gmail.com>
2026-05-18 00:30:30 +0800

    优化 keydao 配置

--- src/copy-keytao.sh
+++ src/copy-keytao.sh
@@ -5,14 +5,26 @@ set -x
 root="$(cd "$(dirname "$0")/.." && pwd)"
 src="$root/src"
 keytao="$src/KeyTao"
+user_dir="$root/user"
+user_keytao_dict="$user_dir/keytao.user.dict.yaml"
+keytao_user_template="$keytao/rime/keytao.user.dict.yaml"
+
+mkdir -p "$user_dir"
+
+if [ ! -s "$user_keytao_dict" ]; then
+  cp "$keytao_user_template" "$user_keytao_dict"
+fi
 
 rsync -aL \
   --exclude="keytao-bj.schema.yaml" \
   --exclude="keytao-dz*.dict.yaml" \
+  --exclude="keytao.user.dict.yaml" \
   --exclude="default.custom.yaml" \
   --exclude="user.yaml" \
   "$keytao/rime/" \
   "$root/"
+
+ln -sfn "user/keytao.user.dict.yaml" "$root/keytao.user.dict.yaml"
 
 rsync -aL \
   --exclude="keytao-dz.schema.yaml" \
